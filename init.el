c48b9e5e3f4d4c4ae1a551e468cde32fd978117b
Jose Miguel Romero Espitia <jose@imagine.ai>
2021-04-30 22:41:15 -0500

    Adding settings for React, TypeScript, TSX, HTML and a little JSX and JavaScript

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 (require 'package)
 
 ;; you can run this file using M-x eval-buffer (M-x ev-b)
@@ -32,7 +33,7 @@
  '(custom-safe-themes
    '("7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" default))
  '(package-selected-packages
-   '(markdown-mode drag-stuff scss-mode org-bullets evil general helpful ivy-rich which-key rainbow-delimiters tide typescript-mode rjsx-mode web-mode anaconda-mode doom-modeline counsel ivy use-package gruvbox-theme)))
+   '(company markdown-mode auto-org-md drag-stuff scss-mode org-bullets evil general helpful ivy-rich which-key rainbow-delimiters tide typescript-mode rjsx-mode web-mode anaconda-mode doom-modeline counsel ivy use-package gruvbox-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -117,7 +118,7 @@
 	 :map minibuffer-local-map
 	 ("C-r" . 'counsel-minibuffer-history))
   :config
-  (setq ivy-initial-inputs-alist nil)) 
+  (setq ivy-initial-inputs-alist nil))
 
 ;; this binds some functions that are already provided by counsel to descibe functions, keys and variables to helpful descriptions that have more information and functionalities, which makes it better for you to understand the functionalities of your configuration
 (use-package helpful
@@ -200,3 +201,74 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
+
+;; library for buffer oriented auto completion
+(use-package company
+  :ensure t
+  :config
+  (setq company-show-numbers t)
+  (setq company-tooltip-align-annotations t)
+
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode))
+
+
+;; syntax checking
+(use-package flycheck
+  :config
+  (global-flycheck-mode))
+
+;; React + TypeScript settings
+
+;; Following tide default settings
+(defun setup-tide-mode ()
+  (interactive) ;; Making the function callable by M-x for example
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+
+  (company-mode +1))
+
+;; HTML rendering
+(use-package web-mode
+  :ensure t
+  :mode (("\\.html?\\'" . web-mode)
+	 ("\\.tsx\\'" . web-mode)
+	 ("\\.jsx\\'" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2
+	web-mode-css-indent-offset 2
+	web-mode-code-indent-offset 2
+	web-mode-block-padding 2
+	web-mode-comment-style 2
+
+	web-mode-enable-css-colorization t
+	web-mode-enable-auto-pairing t
+	web-mode-enable-comment-keywords t
+	web-mode-enable-current-element-highlight t
+	)
+  (add-hook 'web-mode-hook
+	    (lambda ()
+	      (when (string-equal "tsx" (file-name-extension buffer-file-name))
+		(setup-tide-mode))))
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
+
+(use-package typescript-mode
+  :ensure t
+  :config
+  (setq typescript-indent-level 2)
+  (add-hook 'typescript-mode #'subword-mode))
+
+(use-package tide
+  :init
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+	 (typescript-mode . tide-hl-identifier-mode)
+	 (before-save . tide-format-before-save)))
+
+(provide 'init)
+;;; init ends here
